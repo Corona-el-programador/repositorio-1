08bd2da2d9696632f888e1c4908c8ef831b5aa30
Corona-el-programador <117482002+Corona-el-programador@users.noreply.github.com>
2022-11-04 17:46:52 -0500

    Add files via upload

---
+++ python/programa simple.py
@@ -0,0 +1,18 @@
+#programa simple en python
+
+def comprobarcantidades():
+    if a == 0 or b == 0:
+        print("ERROR: no se puede dividir entre 0")
+    else:
+        print(a/b)
+
+print("-Calculadora simple de divisiones-")
+
+a = int(input("Inserte el primer valor"))
+b = int(input("Inserte el siguiente valor"))
+
+
+comprobarcantidades()
+    
+
+
